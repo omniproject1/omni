c728d5132f0a291b77da211faecb79b81d79ae52
shashank cheema <cheema.shashank@gmail.com>
2016-08-01 16:06:27 -0400

    mmm

---
+++ omindata/src/com/omni/data/vo/SunInfoDAO.java
@@ -0,0 +1,45 @@
+package com.omni.data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+import com.omni.data.vo.SunInfoVO;
+
+public class UserInfoDAO {
+	
+	private Connection conn;
+	
+	public SunInfoDAO(Connection conn) {
+		this.conn = conn;
+	}
+
+	public String addSub(SunInfoVO user) {
+		String error = null;	
+		try {
+        	String sql= "INSERT INTO sub_reg (username,passowrd,semester,branch,num_subjects,sub1,sub2,sub3,sub4,sub5) "
+        			+ "values(?,?,?,?,?,?,?,?,?,?,?)";
+        	PreparedStatement ps = conn.prepareStatement(sql);
+        	ps.setString(1, user.getUsertName());
+        	ps.setString(2, user.getPassowrd());
+        	ps.setString(3, user.getSemester());
+        	ps.setString(4, user.getBranch());
+        	ps.setString(5, user.getNumSubjects());
+        	ps.setString(6, user.getSub1());
+        	ps.setString(7, user.getSub2());
+        	ps.setString(8, user.getSub3() );
+        	ps.setString(9, user.getSub4());
+        	ps.setString(10, user.getSub5());
+        	int rcode = ps.executeUpdate();
+        	error = ""+rcode;
+		} catch (Exception ex){
+			error = ex.getMessage();
+			ex.printStackTrace();
+		}		
+		return error;
+	}
+
+
+
+}
---
+++ omnicore/src/com/omni/core/UserInfoBusiness.java
@@ -0,0 +1,27 @@
+spackage com.omni.core;
+
+import com.omni.data.SigninDatabaseAdapter;
+import com.omni.data.vo.UserInfoVO;
+
+public class UserInfoBusiness {
+	
+	public String updateUserInfo(UserInfoVO userVO) {
+		String error = null;
+		// 1: check whether this user already exists in database
+		SigninDatabaseAdapter adapter = new SigninDatabaseAdapter();
+		if (adapter.isUserExists(userVO.getUserName(), userVO.getFirstName(), userVO.getLastName())) { 
+			error = adapter.updateUser(userVO);
+		} else {
+			error = adapter.addUser(userVO);	
+		}
+		return error;
+	}
+	
+	public String updateSubInfo(SunInfoVO userVO) {
+		String error = null;
+		// 1: check whether this user already exists in database
+		SigninDatabaseAdapter adapter = new SigninDatabaseAdapter();
+		error=adapter.addSub(userVo)
+		return error;
+	}
+}
